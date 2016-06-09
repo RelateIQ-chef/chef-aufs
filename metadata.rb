37d1ade2a819522fee251902a3a4755d85ad2154
Brian Flad <bflad417@gmail.com>
2016-06-08 22:06:45 -0400

    Make TravisCI happier: add urls to metadata, remove extraneous Rubocop config

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,10 @@ license 'Apache 2.0'
 description 'Installs/Configures AUFS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.1'
+
+source_url 'https://github.com/bflad/chef-aufs'
+issues_url 'https://github.com/bflad/chef-aufs/issues'
+
 recipe 'aufs', 'Installs/Configures AUFS'
 recipe 'aufs::module', 'Loads AUFS Linux module'
 recipe 'aufs::package', 'Installs AUFS via package'
